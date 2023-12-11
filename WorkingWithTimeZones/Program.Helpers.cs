57d731feb6054b0f83ba45cf8c42e11067e00b4c
Kamil Łukasik <poczta.lukasik@gmail.com>
2023-12-11 13:52:43 +0100

    #TimeZones

---
+++ WorkingWithTimeZones/Program.Helpers.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel; // ReadOnlyCollection<T>
+
+partial class Program
+{
+    static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previousColor;
+    }
+
+    static void OutputTimeZones()
+    {
+        // get the time zones reqistered with the OS
+        ReadOnlyCollection<TimeZoneInfo> zones = TimeZoneInfo.GetSystemTimeZones();
+
+        WriteLine("*");
+        WriteLine($"* {zones.Count} time zones:");
+        WriteLine("*");
+
+        // order the time zones by Id instead of DisplayName
+        foreach (TimeZoneInfo zone in zones.OrderBy(z => z.Id))
+        {
+            WriteLine($"{zone.Id}");
+        }
+    }
+
+    static void OutputDateTime(DateTime dateTime, string title)
+    {
+        SectionTitle(title);
+        WriteLine($"Value: {dateTime}");
+        WriteLine($"Kind: {dateTime.Kind}");
+        WriteLine($"IsDaylightSavingTime: {dateTime.IsDaylightSavingTime()}");
+        WriteLine($"ToLocalTime: {dateTime.ToLocalTime()}");
+        WriteLine($"ToUniversalTime: {dateTime.ToUniversalTime()}");
+    }
+
+    static void OutputTimeZone(TimeZoneInfo zone, string title)
+    {
+        SectionTitle(title);
+        WriteLine($"Id: {zone.Id}");
+        WriteLine("IsDaylightSavingTime(DateTime.Now): {0}",zone.IsDaylightSavingTime(DateTime.Now));
+        WriteLine($"StandardName: {zone.StandardName}");
+        WriteLine($"DaylightName: {zone.DaylightName}");
+        WriteLine($"BaseUtcOffset: {zone.BaseUtcOffset}");
+    }
+
+    static string GetCurrentZoneName(TimeZoneInfo zone, DateTime when)
+    {
+        return zone.IsDaylightSavingTime(when) ? zone.DaylightName : zone.StandardName;
+    }
+}
+
