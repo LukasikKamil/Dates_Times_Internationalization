c987855e86bf8d73ba9e34502317867b53e7f113
Kamil Łukasik <poczta.lukasik@gmail.com>
2023-12-13 21:54:41 +0100

    #Cultures

---
+++ WorkingWithCultures/AiOResources.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Localization; // IStringLocalizer, LocalizedString
+
+public class AiOResources
+{
+    private readonly IStringLocalizer<AiOResources> localizer = null!;
+
+    public AiOResources(IStringLocalizer<AiOResources> localizer)
+    {
+        this.localizer = localizer;
+    }
+
+    public string? GetEnterYourNamePrompt()
+    {
+        string resourceStringName = "EnterYourName";
+
+        // 1. get the LocalizedString object
+        LocalizedString localizedString = localizer[resourceStringName];
+
+        // 2. check if the resource string was found
+        if (localizedString.ResourceNotFound)
+        {
+            ConsoleColor previousColor = ForegroundColor;
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine($"Error: resource string \"{resourceStringName}\" not found." 
+                + Environment.NewLine
+                + $"Search path: {localizedString.SearchedLocation}");
+            ForegroundColor = previousColor;
+
+            return $"{localizedString}: ";
+        }
+        // 3. return the found resource string
+        return localizedString;
+    }
+
+    public string? GetEnterYourDobPrompt()
+    {
+        // LocalizedString has an implicid cast to string
+        // that falls back to the key if the resource string is not found
+        return localizer["EnterYourDob"];
+    }
+
+    public string? GetEnterYourSalaryPrompt()
+    {
+        return localizer["EnterYourSalary"];
+    }
+
+    public string? GetPersonDetails(string name, DateTime dob, int minutes, decimal salary)
+    {
+        return localizer["PersonDetails", name, dob, minutes, salary];
+    }
+}
+
---
+++ WorkingWithCultures/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.Hosting; // IHost, Host
+
+// AddLocxalization, AddTransient<T>
+using Microsoft.Extensions.DependencyInjection;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddLocalization(options =>
+        {
+            options.ResourcesPath = "Resources";
+        });
+
+        services.AddTransient<AiOResources>();
+    })
+    .Build();
+
+// to enable special characters like "euro"
+OutputEncoding = System.Text.Encoding.Unicode;
+
+OutputCultures("Current culture");
+
+WriteLine("Example ISO culture codes:");
+
+string[] cultureCodes = new[]
+{
+    "da-DK", "en-GB", "en-US", "fa-IR", "fr-CA", "fr-FR", "he-IL", "pl-PL", "sl-SI"
+};
+
+foreach (string code in cultureCodes)
+{
+    CultureInfo culture = CultureInfo.GetCultureInfo(code);
+    WriteLine("  {0}: {1} / {2}",
+        culture.Name, culture.EnglishName, culture.NativeName);
+}
+
+WriteLine();
+
+Write("Enter an ISO culture code: ");
+string? cultureCode = ReadLine();
+
+if (string.IsNullOrWhiteSpace(cultureCode))
+{
+    cultureCode = "en-US";
+}
+
+CultureInfo ci;
+
+try
+{
+    ci = CultureInfo.GetCultureInfo(cultureCode);
+}
+catch (CultureNotFoundException)
+{
+    WriteLine($"Culture code not found: {cultureCode}");
+    WriteLine("Exiting the app.");
+    return;
+}
+
+// change the current cultures on the thread
+CultureInfo.CurrentCulture = ci;
+CultureInfo.CurrentUICulture = ci;
+
+OutputCultures("After changing the current culture");
+
+AiOResources resources = host.Services.GetRequiredService<AiOResources>();
+
+Write(resources.GetEnterYourNamePrompt());
+string? name = ReadLine();
+if (string.IsNullOrWhiteSpace(name))
+{
+    name = "Bob";
+}
+
+Write(resources.GetEnterYourDobPrompt());
+string? dobText = ReadLine();
+if (string.IsNullOrWhiteSpace(dobText))
+{
+    // if they not enter a DOB then use sensible defaults for their culture
+    dobText = ci.Name switch
+    {
+        "en-US" or "fr-CA" => "1/27/1990",
+        "da-DK" or "fr-FR" or "pl-PL" => "27/1/1990",
+        "fa-IR" => "1990/1/27",
+        _ => "1/27/1990"
+    };
+}
+
+Write(resources.GetEnterYourSalaryPrompt());
+string? salaryText = ReadLine();
+if(string.IsNullOrWhiteSpace(salaryText))
+{
+    salaryText = "34500";
+}
+
+DateTime dob = DateTime.Parse(dobText);
+int minutes = (int)DateTime.Today.Subtract(dob).TotalMinutes;
+decimal salary = decimal.Parse(salaryText);
+
+WriteLine(resources.GetPersonDetails(name, dob, minutes, salary));
